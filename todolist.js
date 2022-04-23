dd039007a4b8d45eb4d7fc28df80dde7bee8e6c4
Bharathraj <96296825+Bharathraj75@users.noreply.github.com>
2022-04-24 00:33:50 +0530

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,1 +1,37 @@
 
+function double(){
+    console.log("Double clicked");
+}
+
+
+const todolist =document.getElementById('todotext');
+const todoUl=document.getElementById('todoul');
+function addtask(){
+    if(todolist.value.trim() === " ")
+
+    {
+        alert("Enter Something in list");
+    }
+    else
+    {
+        console.log(todolist.value);
+        const liTag =document.createElement('li');
+        const spanText=document.createElement('span');
+        const textNode=document.createTextNode(todolist.value.trim());
+        spanText.appendChild(textNode);
+        const spanClose=document.createElement("button");
+        spanClose.classList.add('close');
+        spanClose.innerHTML='&times;';
+        liTag.appendChild(spanText);    
+        liTag.appendChild(spanClose);  
+        todoUl.appendChild(liTag);
+        todolist.value=' ';
+        spanClose.addEventListener('click',(a)=>
+        {
+            const li = a.target.parentElement;
+            li.parentElement.removeChild(li);
+        });
+    }
+    
+   
+}
